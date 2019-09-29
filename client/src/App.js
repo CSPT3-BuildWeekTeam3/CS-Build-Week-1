96bc7f0a468a614cca505c0347921683aaa7fa8e
meganfontenot <meganmini@Mac-mini.local>
2019-09-29 12:00:58 -0500

    Setting up basic component flow

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import '../node_modules/react-vis/dist/style.css';
+import {XYPlot, LineSeries} from 'react-vis';
 import { Route } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 
-function App() {
-  return (
-    <div className="App">
-      <Route exact path="/" component={Home} />
-      <Route path='/login' component={Login} />
-    </div>
-  );
-}
+ function App() {
+   return (
+     <div className="App">
+       <Route exact path="/" component={Home} />
+       <Route path='/login' component={Login} />
+     </div>
+   );
+ }
 
-export default App;
+ export default App;
+

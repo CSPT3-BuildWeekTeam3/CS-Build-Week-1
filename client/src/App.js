375d53fa6cf743a850d1552f8d2460a9982df360
meganfontenot <meganmini@Mac-mini.local>
2019-09-29 15:24:35 -0500

    initial data from server

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,64 @@
-import React, { Component } from 'react';
-import './App.css';
-import '../node_modules/react-vis/dist/style.css';
-import {XYPlot, LineSeries} from 'react-vis';
-import { Route } from 'react-router-dom'
-import Login from './components/Login'
-import Home from './components/Home'
+import React from 'react';
+import NavBar from './components/NavBar';
+import Login from './components/Login';
+import ChatBox from './components/ChatBox';
+import Dungeon from './components/Dungeon';
+import RoomInfo from './components/RoomInfo';
+import Commands from './components/Commands';
+import { CssBaseline, Container } from '@material-ui/core';
+import axios from 'axios';
 
- function App() {
-   return (
-     <div className="App">
-       <Route exact path="/" component={Home} />
-       <Route path='/login' component={Login} />
-     </div>
-   );
- }
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      loggedIn: false,
+      currentRoom: {}
+    };
 
- export default App;
+    this.content = {
+      headers: {
+        Authorization: 'Token 6b7b9d0f33bd76e75b0a52433f268d3037e42e66'
+      }
+    };
+  }
 
+  componentDidMount() {
+    axios
+      .get('https://cspt3-buildweek-backend.herokuapp.com/api/adv/init', this.content)
+      .then(data => {
+        this.setState({currentRoom: data.data})
+
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  tempChangeLogin = () => {
+    this.setState(prev => {
+      return { loggedIn: !prev.loggedIn };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <CssBaseline />
+        <NavBar tempChangeLogin={this.tempChangeLogin} />
+        {!this.state.loggedIn ? (
+          <Login />
+        ) : (
+          <Container>
+            <Dungeon />
+            <ChatBox />
+            <Commands />
+            <RoomInfo currentRoom={this.state.currentRoom} />
+          </Container>
+        )}
+      </div>
+    );
+  }
+}
+
+export default App;
